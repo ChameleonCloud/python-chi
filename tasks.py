482d285abf7ca22eeddff39a8c52eedfae7d9014
Nick Timkovich <prometheus235@gmail.com>
2017-02-15 19:43:40 -0600

    Tag on publish

--- tasks.py
+++ tasks.py
@@ -3,12 +3,23 @@ from __future__ import print_function
 import os
 import errno
 import shutil
-from six.moves import urllib
+from six.moves import input, urllib
 
 from invoke import task
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 PACKAGE_NAME = 'hammers'
+
+
+def get_version():
+    with open('hammers/__init__.py') as f:
+        for line in f:
+            if line.startswith('__version__'):
+                _, version = line.split('=')
+                version = version.strip().strip('\'\"')
+                return version
+        else:
+            raise RuntimeError('couldn\'t find version')
 
 
 @task
@@ -43,4 +54,8 @@ def install(ctx):
 
 @task
 def publish(ctx):
+    version = get_version()
+    tag_info = input('Enter tag description for "v{}":\n'.format(version))
+
     ctx.run('twine upload dist/*')
+    ctx.run('git tag -a v{} -m {}'.format(version, tag_info))
