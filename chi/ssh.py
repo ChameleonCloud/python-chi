5673dbd2cf84acf9b3faa31cb7f0b8c4b3fe52ec
Nick Timkovich <npt@uchicago.edu>
2017-04-13 11:00:43 -0500

    Capture errors separately (some weren't working?)

--- chi/ssh.py
+++ chi/ssh.py
@@ -45,40 +45,54 @@ def wait(host, username, callback='dots'):
         if not isinstance(callback, collections.Callable):
             raise ValueError("callback isn't callable.")
 
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.WarningPolicy())
-
     for attempt in range(150):
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.WarningPolicy())
         try:
             client.connect(host, username=username, timeout=10)
 
-        except expected_wait_errors as e:
-            if isinstance(e, OSError):
-                # we only want to capture "network unreachable"
-                if e.errno != errno.ENETUNREACH:
-                    raise
-
-            for exc_type in error_counts:
-                if type(e) == exc_type:
-                    error_counts[exc_type] += 1
-                    break
-            else:
-                sub_errors += 1
-                print('semi-unexpected error:', type(e), str(e))
-
-            if error_counts[paramiko.AuthenticationException] > 5:
-                raise RuntimeError('auth not working')
-
-            if error_counts[socket.timeout] > 100:
-                raise RuntimeError('timed out too much')
+        # except expected_wait_errors as e:
+        #     if isinstance(e, OSError):
+        #         # we only want to capture "network unreachable"
+        #         if e.errno != errno.ENETUNREACH:
+        #             raise
+        #
+        #     for exc_type in error_counts:
+        #         if type(e) == exc_type:
+        #             error_counts[exc_type] += 1
+        #             break
+        #     else:
+        #         sub_errors += 1
+        #         print('semi-unexpected error:', type(e), str(e))
+        #
+        #     if error_counts[paramiko.AuthenticationException] > 5:
+        #         raise RuntimeError('auth not working')
+        #
+        #     if error_counts[socket.timeout] > 100:
+        #         raise RuntimeError('timed out too much')
+        except paramiko.AuthenticationException as e:
+            # while the ssh service starting, it can accept connections but auth isn't fully set.
+            pass
+        except paramiko.SSHException as e:
+            # local interruptions?
+            pass
+        except paramiko.ssh_exception.NoValidConnectionsError as e:
+            # server might be down while starting
+            pass
+        except socket.timeout as e:
+            # if the floating IP is still kinda floating and not getting routed.
+            pass
+        except OSError as e:
+            # filter so only capturing errno.ENETUNREACH
+            if e.errno != errno.ENETUNREACH:
+                raise
         else:
+            print('<wait over>')
             break
-
         finally:
             client.close()
 
         callback(attempt)
-
         time.sleep(10)
 
 
