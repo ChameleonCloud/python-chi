567ac859df07a1b5d4096201ff4459a6aa2d9adc
Jason <jasonanderson@uchicago.edu>
2018-09-10 14:11:45 -0500

    Add more starting tests

--- chi/context.py
+++ chi/context.py
@@ -1,35 +1,41 @@
-from keystoneauth1 import adapter, session
+from keystoneauth1.adapter import Adapter
+from keystoneauth1.session import Session
 from keystoneauth1.identity import v3
 from os import getenv
 
-_context = {}
+_overrides = {}
+_defaults = {}
 _keys = [
     'auth_url',     # THe OpenStack Keystone authentication URL
     'key_filename', # The path to the SSH private key
     'key_name',     # The OpenStack SSH key name
     'image',        # The OpenStack image name
     'project_name', # The OpenStack project name
+    'region_name',  # The OpenStack region name
     'token',        # A valid OpenStack auth token
 ]
 
 # Automatically set context from environment.
 for key in _keys:
-    _context[key] = getenv('OS_{}'.format(key.upper()))
+    _defaults[key] = getenv('OS_{}'.format(key.upper()))
+
+def reset():
+    _overrides = {}
 
 def set(key, value):
     if not key in _keys:
         raise KeyError('Unknown setting "{}"'.format(key))
-    _context[key] = value
+    _overrides[key] = value
 
 def get(key, default=None):
     if not key in _keys:
         raise KeyError('Unknown setting "{}"'.format(key))
-    return _context.get(key, default)
+    return _overrides.get(key, _defaults.get(key, default))
 
 def session():
-    auth = v3.Token(auth_url=fetch('auth_url'),
-                    token=fetch('token'),
-                    project_name=fetch('project_name'),
+    auth = v3.Token(auth_url=get('auth_url'),
+                    token=get('token'),
+                    project_name=get('project_name'),
                     project_domain_name='default')
-    sess = session.Session(auth=auth)
-    return adapter.Adapter(session=sess, region_name=fetch('region_name'))
+    sess = Session(auth=auth)
+    return Adapter(session=sess, region_name=get('region_name'))
--- tests/test_context.py
+++ tests/test_context.py
@@ -1,9 +1,18 @@
 import chi
-import unittest
+import pytest
 
-class Context(unittest.TestCase):
+def setup_function():
+    chi.reset()
 
-    def test_fetch(self):
-        value = 'KEYNAME'
-        chi.set('key_name', value)
-        self.assertEqual(chi.get('key_name'), value)
+def test_fetch():
+    value = 'KEYNAME'
+    chi.set('key_name', value)
+    assert chi.get('key_name') == value
+
+def test_fetch_invalid_key():
+    with pytest.raises(KeyError):
+        chi.get('some_invalid_key')
+
+def test_session():
+    session = chi.session()
+    assert session
