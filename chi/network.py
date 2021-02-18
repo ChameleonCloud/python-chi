cc882f2bd7216a319694dbfd1c5599bf0f988305
mwhicks2 <mwhicks2@jupyter.chameleoncloud.org>
2021-02-18 00:58:44 +0000

    Removed redundant change

--- chi/network.py
+++ chi/network.py
@@ -546,7 +546,7 @@ def remove_all_routes_from_router(router_id):
         router_id (str): The router ID.
     """
     return remove_routes_from_router(router_id,
-        get_router(router_id).get('routes'))
+        get_router(router_id)['routes'])
 
 
 def add_port_to_router(router_id, port_id):
