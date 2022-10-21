3a0f1368aa743745107848a6b257d38d03ea099e
willtholke <willtholke@berkeley.edu>
2022-10-21 13:47:36 -0700

    Add project selector widget

--- chi/widgets.py
+++ chi/widgets.py
@@ -1,5 +1,9 @@
+from os import environ
+
 import ipywidgets as widgets
+import jwt
 import requests
+from IPython.core.display import display
 
 import chi
 
@@ -14,6 +18,13 @@ def get_sites():
 
     sites_ret = api_ret.json().get("items")
     return sites_ret
+
+
+def get_projects():
+    """ Return list of user's projects. """
+    os_token = environ.get("OS_ACCESS_TOKEN")
+    jwt_info = jwt.decode(os_token, options={"verify_signature": False})
+    return jwt_info.get("project_names")
 
 
 def choose_site():
@@ -46,3 +57,33 @@ def choose_site():
     update_selected_site(site_chooser.label)
 
     return widgets.VBox([site_chooser, site_output])
+
+
+def choose_project():
+    """ Return IPyWidget Select object for user to select from
+    list of available projects.
+    """
+    def project_dropdown_callback(change):
+        update_selected_project(change["new"])
+
+    def update_selected_project(project_name):
+        chi.set("project_name", project_name)
+        project_output.clear_output()
+        with project_output:
+            print(f"Using project: {project_name}")
+
+    project_output = widgets.Output()
+    project_names = get_projects()
+    project_chooser = widgets.Select(
+        options=project_names,
+        rows=8,
+        value=project_names[0]
+    )
+
+    # update selected project on callback
+    project_chooser.observe(project_dropdown_callback, names='value')
+
+    # initialize values before selection is made
+    update_selected_project(project_chooser.value)
+
+    return widgets.VBox([project_chooser, project_output])
