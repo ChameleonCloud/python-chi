78d42d88bd22c1e263357d5c512f4ffcfc8b255b
willtholke <willtholke@berkeley.edu>
2022-11-04 18:34:12 -0700

    Completed get_nodes, made progress on choose_node

--- chi/widgets.py
+++ chi/widgets.py
@@ -13,9 +13,8 @@ from .context import get
 
 
 def get_node_ids(site):
-    """ Get the UIDs associated with each node_type for a given site.
-
-    Not currently used.
+    """ Use discovery data to get the UIDs associated with each node_type
+    for a given site.
     """
     discovery = get_discovery(site)
     return [(key, discovery[key]['uid']) for key in discovery.keys()]
@@ -27,177 +26,151 @@ def get_site():
 
 
 def get_discovery(site_name: str = None):
-    """ GET Chameleon resource registry node data for a specific site or
-    all sites. Returns the name of the site(s), each node in that site(s),
-    and the data associated with that node.
+    """ Get Chameleon resource registry node data for all sites or a single
+    specific site. Returns the name of the site(s), each node in that site(s),
+    and the data associated with that node. The data associated with each node
+    should be indexed into by node name. Note that CHI@Edge is an exception.
 
     Returns:
-        { 'uid' : { 'node_type': { discovery data} } }
+        if site_name:
+            { 'site_name' : { 'node_name': { discovery data } } }
+
+        if not site_name:
+            { 'node_name': { discovery data } }
     """
-    if site_name == 'CHI@Edge':  # handle edge case
+    if site_name == 'CHI@Edge':
         return None
     r = requests.get('https://api.chameleoncloud.org/sites/')
-    jsonified = json.loads(r.text)
-    name_uid = {jsonified['items'][i]['name']: jsonified['items'][i]['uid']
-                for i in range(len(jsonified['items']))}
+    r_json = json.loads(r.text)
+    name_uid = {r_json['items'][i]['name']: r_json['items'][i]['uid']
+                for i in range(len(r_json['items']))}
 
-    if site_name:  # get site-specific discovery data
+    if site_name:
         if site_name not in name_uid:
             raise KeyError(f'{site_name} is an invalid site name')
         r = requests.get('https://api.chameleoncloud.org/sites/' +
                          name_uid[site_name] + '/clusters/chameleon/nodes')
         data = json.loads(r.text)['items']
-        return {data[i]['node_type']: data[i] for i in range(len(data))}
+        return {data[i]['node_name']: data[i] for i in range(len(data))}
 
-    discovery_data, nodes = {}, {}
-    for count, name_uid in enumerate(name_uid.items(), 0):  # get all discovery data
+    discovery_data = {}
+    for count, name_uid in enumerate(name_uid.items(), 0):
         name, uid = name_uid
         if uid == "edge":
             continue
         r = requests.get('https://api.chameleoncloud.org/sites/' + uid +
-                            '/clusters/chameleon/nodes')
+                         '/clusters/chameleon/nodes')
         data = json.loads(r.text)['items'][count]
-        node_type = data['node_type']
-        if node_type not in nodes.keys():
-            nodes[node_type] = data
-            continue
-        nodes[node_type] = data
-        discovery_data[name] = nodes
-
+        discovery_data[name] = {data['node_name']: data}
     return discovery_data
 
 
 def get_nodes(display=True):
-    """ Compute a tuple of available/unavailable nodes and their data
-    and display availability for all nodes.
+    """ Construct tuples of node availability for all nodes. By default,
+    display availability for all nodes; otherwise, return the data itself.
 
     Returns:
         if display=True:
-            pandas df
+            pandas df with cols('Type', 'In Use', 'Free')
         if display=False:
-            ( { "avail_node": "blazar/disco data" }, { "unavail_node":
-            "blazar/disco data" }
+            ( { "avail_node": "disco data" }, { "unavail_node": disco data" } )
     """
-    client = chi.blazar()
-    allocations = client.host.list_allocations()
-    hosts, states = {host["id"]: host for host in client.host.list()}, {}
-    for alloc in allocations:
-        uid = alloc["resource_id"]
-        host = hosts[uid]
-        host["reservations"] = alloc["reservations"]
-        states[uid] = host
-
-    all_nodes, unavailable_nodes, available_nodes = {}, {}, {}
-
+    all_nodes, unavail_nodes, avail_nodes = {}, {}, {}
     discovery = get_discovery(get_site())
-    # Assume all unavail, then remove from set if avail
-    for uid, blazar_data in hosts.items():
-        node_type = blazar_data['node_type']
-        free = blazar_data['reservable']
-        unavailable_nodes[node_type] = [free, blazar_data['reservations']]
+    client = chi.blazar()
+    hosts = {host["hypervisor_hostname"]: host for host in client.host.list()}
 
-        # Initialize dict schema: { "node_type" : (# avail, # unavail) }
+    for uid, blazar_data in hosts.items():
+        node_name = blazar_data['node_name']
+        node_type = discovery[node_name]['node_type']
+        free = blazar_data['reservable']
+
+        # initialize dict schema: { "node_type" : (# avail, # unavail) }
         all_nodes.setdefault(node_type, (0, 0))
         all_nodes[node_type] = (all_nodes[node_type][0] + free,
                                 all_nodes[node_type][1] + (not free))
-        # update available/unavailable nodes accordingly
-        if all_nodes[node_type][0]:
-            print(node_type)
-            available_nodes[node_type] = unavailable_nodes[node_type]
-            unavailable_nodes.pop(node_type)
 
-    # Display availability for all nodes if requested (default)
+        node_data = discovery[node_name]
+        if all_nodes[node_type][0]:
+            avail_nodes[node_type] = node_data
+        else:
+            unavail_nodes[node_type] = node_data
+
     if display:
         num_avail, num_unavail = map(list, zip(*all_nodes.values()))
-        list(all_nodes.values())
         d = {'Type': list(all_nodes.keys()),
              'In Use': num_avail,
              'Free': num_unavail}
         return pd.DataFrame(data=d)
 
-    return available_nodes, unavailable_nodes
+    return avail_nodes, unavail_nodes
 
 
-def custom_type_error(expected, received):
-    raise TypeError(f"Expected type '{expected.__name__}', got "
-                    f"'{received.__name__}' instead")
-
-
-def choose_node(gpu: bool = None, gpu_count: int = None, ssd: bool = None,
-                storage_size_gb: int = None, architecture: str = None):
+def choose_node(gpu: bool = None, gpu_count: int = None,
+                ssd: bool = None, storage_size_gb: int = None,
+                architecture: str = None):
     """ Return IPyWidget Select object for user to select from
-    list of available nodes.
+    list of available nodes of the given parameters.
 
     Parameters: (all optional, default None)
-
-    Display helpful information (TBD) when user selects a node.
     """
-    # TODO: replace temporary arg typechecking
-    if type(gpu) is not bool and gpu is not None:
-        custom_type_error(bool, type(gpu))
-    if type(gpu_count) is not int and gpu_count is not None:
-        custom_type_error(int, type(gpu_count))
-    if type(storage_size_gb) is not int and storage_size_gb is not None:
-        custom_type_error(int, type(storage_size_gb))
-    if type(ssd) is not bool and ssd is not None:
-        custom_type_error(bool, type(ssd))
-    if type(architecture) is not str and architecture is not None:
-        custom_type_error(str, type(architecture))
-    if gpu_count and gpu_count < 0:
-        print("Gpu count too low")
-        # TODO: raise some error
-
     def node_dropdown_callback(change):
         update_selected_node(change["new"])
 
     def update_selected_node(node_type):
         node_output.clear_output()
         with node_output:
-            chi.use_node(node_type, available_nodes[node_type])
+            chi.use_node(node_type, avail_nodes[node_type])
 
-    # Get all available nodes and define ret_nodes
-    available_nodes, ret_nodes = get_nodes(display=False)[1], {}
+    avail_nodes = get_nodes(display=False)[0]
 
-    # Assume that if someone queries for # gpus, they want nodes with GPUs
-    if gpu_count:
-        gpu = True
-        # TODO: Implement GPU_COUNT argument logic
+    def find_gpu(nodes, count: int = None):
+        """ Find all nodes with GPUs, and restrict the set to nodes with
+        a certain number of GPUs if specified.
+        """
+        # find all with GPUs
+        if count:
+            print("Implement me")
+            # out of those with GPUs, find with GPU == count GPUs
 
-    # GPU argument logic
-    if gpu is None:  # default
-        print("Displaying all available nodes:")
-        ret_nodes = available_nodes
+        return nodes  # TODO: Change me
+
+    # GPU_COUNT logic
+    if gpu_count is None:
+        pass
+    elif gpu_count > 0:
+        print("gpu_count is > 0")
+        avail_nodes = find_gpu(avail_nodes, gpu_count)
+    elif gpu_count == 0:
+        print("gpu_count is 0 ")
+        if gpu is True:
+            raise TypeError("Can't have gpu=True, gpu_count=0")
     else:
-        gpu_nodes, non_gpu_nodes = {}, {}
-        for node, data in available_nodes.items():
-            try:
-                if data['gpu.gpu'] == 'True':
-                    gpu_nodes[node] = data
-                if data['gpu.gpu'] == 'False':
-                    non_gpu_nodes[node] = data
-            except KeyError:
-                # if gpu key does not exist, assume that it has no gpu
-                non_gpu_nodes[node] = data
-                continue
-        if gpu:
-            print("Displaying all nodes with GPUs:")
-            ret_nodes = gpu_nodes
-        elif not gpu:
-            print("Displaying all nodes without GPUs:")
-            ret_nodes = non_gpu_nodes
+        raise TypeError(f"Invalid parameter gpu_count={gpu_count}")
 
-    # TODO: Implement storage_size_gb argument logic
-    # TODO: Implement ssd argument logic
-    # TODO: Implement architecture argument logic
+    # GPU logic
+    if gpu is None:
+        pass
+    elif gpu is True and gpu_count is None:
+        print("gpu is true and gpu_count is none")
+        avail_nodes = find_gpu(avail_nodes)
+    elif gpu is False:
+        print("gpu is false")
+        avail_nodes = find_gpu(avail_nodes, 0)
+    else:
+        raise TypeError(f"Invalid parameter gpu={gpu}")
 
-    # Display message and exit if all nodes unavailable
-    if not list(ret_nodes.keys()):
+    # TODO: implement STORAGE_SIZE_GB
+    # TODO: implement SSD
+    # TODO: implement architecture
+
+    if not list(avail_nodes.keys()):
         print("All nodes of the given parameters are currently reserved. "
               "Please try again later.")
         return
 
     node_output = widgets.Output()
-    node_chooser = widgets.Select(options=ret_nodes.keys())
+    node_chooser = widgets.Select(options=avail_nodes.keys())
 
     # update selected note on callback
     node_chooser.observe(node_dropdown_callback, names='value')
