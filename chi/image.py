6221e024f0568fd971d65eae4f83897c26b56351
Mark Powers <markpowers@uchicago.edu>
2025-06-18 10:20:07 -0500

    Fix formatting

--- chi/image.py
+++ chi/image.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import List, Optional
 
-from glanceclient.exc import NotFound, HTTPBadRequest
+from glanceclient.exc import HTTPBadRequest, NotFound
 from packaging.version import Version
 
 from chi import context
