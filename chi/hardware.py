1ca49d01b152ddc069e1ed8181ba021c2a109fad
Nicole Brewer <brewer36@purdue.edu>
2025-06-19 13:38:53 -0700

    Improved attribute checking

--- chi/hardware.py
+++ chi/hardware.py
@@ -120,19 +120,35 @@ class Node:
         
         if not self.reservable:
             reservable_style['background'] = '#f69084'
-    
-        box = Box(children=[
+
+        children=[
             HTML(f"<b>Node Name:</b> {self.name}", style=style, layout=layout),
             HTML(f"<b>Site:</b> {self.site}", style=style, layout=layout),
-            HTML(f"<b>Type:</b> {self.type}", style=style, layout=layout),
-            HTML(f"<b>Clock Speed:</b> {self.cpu['clock_speed'] / 1e9:.2f} GHz", style=style, layout=layout),
-            HTML(f"<b>RAM:</b> {self.main_memory['humanized_ram_size']}", style=style, layout=layout),
-            HTML(f"<b>GPU Model:</b> {self.gpu['gpu_model'] or 'None'}", style=style, layout=layout),
-            HTML(f"<b>GPU Count:</b> {self.gpu['gpu_count']}", style=style, layout=layout),
-            HTML(f"<b>Storage Size:</b> {self.storage_devices[0]['humanized_size']}", style=style, layout=layout),
-            HTML(f"<b>Reservable:</b> {'Yes' if self.reservable else 'No'}", style=reservable_style, layout=layout),
-        ])
+            HTML(f"<b>Type:</b> {self.type}", style=style, layout=layout)
+        ]
+        if getattr(self, 'cpu', False) and 'clock_speed' in self.cpu:
+            children.append(HTML(f"<b>Clock Speed:</b> {self.cpu['clock_speed'] / 1e9:.2f} GHz", style=style, layout=layout))
+        
+        if getattr(self, 'main_memory', False) and 'humanized_ram_size' in self.main_memory:
+            children.append(HTML(f"<b>RAM:</b> {self.main_memory['humanized_ram_size']}", style=style, layout=layout))
+        
+        if getattr(self, 'gpu', False) and 'gpu' in self.gpu and self.gpu['gpu']:
+            if 'gpu_count' in self.gpu:
+                children.append(HTML(f"<b>GPU Count:</b> {self.gpu['gpu_count']}", style=style, layout=layout))
+            else:
+                children.append(HTML(f"<b>GPU:</b> True", style=style, layout=layout))
+            if 'gpu_model' in self.gpu:
+                children.append(HTML(f"<b>GPU Model:</b> {self.gpu['gpu_model']}", style=style, layout=layout))
+        else:
+            children.append(HTML(f"<b>GPU Count:</b> 0", style=style, layout=layout))
+        
+        if getattr(self, 'storage_devices', False) and len(self.storage_devices) > 0 and 'humanized_size' in self.storage_devices[0]:
+            children.append(HTML(f"<b>Storage Size:</b> {self.storage_devices[0]['humanized_size']}", style=style, layout=layout))
+
+        if getattr(self, 'reservable', False):
+            children.append(HTML(f"<b>Reservable:</b> {'Yes' if self.reservable else 'No'}", style=reservable_style, layout=layout))
     
+        box = Box(children=children)
         box.layout=Layout(flex_flow='row wrap')
         display(box)
 
